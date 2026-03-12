8e48347c7c5319487dd300304384802ad102e2ff
Hugh Wang <ihavenoname111@hotmail.com>
2026-03-12 16:21:15 +1300

    [local.bin] fix an error message

--- .local/bin/stop_llm.sh
+++ .local/bin/stop_llm.sh
@@ -2,6 +2,9 @@
 set -e
 
 while IFS= read -r container_name; do
+    if [ -z "$container_name" ]; then
+        continue
+    fi
     podman container stop "$container_name"
 done <<< "$(podman container ls --filter label=llm --format "{{.Names}}")"
 
