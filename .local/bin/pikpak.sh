983a3cc4e32022d1e0c262f8c5a39253df1600f5
Hugh Wang <ihavenoname111@hotmail.com>
2026-03-22 20:21:18 +1300

    [local.bin] Auto choose path for pikpak

--- .local/bin/pikpak.sh
+++ .local/bin/pikpak.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-"$HOME/.local/bin/wine-net.sh" 'C:\Program Files\PikPak\PikPak.exe'
+if [ -f "$HOME/.wine/net/drive_c/Program Files (x86)/PikPak/PikPak.exe" ]; then
+    "$HOME/.local/bin/wine-net.sh" 'C:\Program Files (x86)\PikPak\PikPak.exe'
+
+elif [ -f "$HOME/.wine/net/drive_c/Program Files/PikPak/PikPak.exe" ]; then
+    "$HOME/.local/bin/wine-net.sh" 'C:\Program Files\PikPak\PikPak.exe'
+
+else
+    notify-send "Cannot find PikPak.exe"
+fi
