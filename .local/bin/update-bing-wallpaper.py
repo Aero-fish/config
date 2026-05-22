8ee85092da7747299878fa9cf6357ba8035b245b
Hugh Wang <ihavenoname111@hotmail.com>
2026-05-23 02:29:27 +1200

    refactor(update-bing-wallpaper)

--- .local/bin/update-bing-wallpaper.py
+++ .local/bin/update-bing-wallpaper.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-from typing import Tuple
 from urllib import request
 import contextlib
 import datetime
@@ -25,16 +24,15 @@ def set_wallpaper():
     wallpaper_path = downloaded_wallpaper[-1]
 
     if sys.platform.startswith("win32"):
-        cmd = (
-            "REG ADD \"HKCU\\Control Panel\\Desktop\" /v Wallpaper /t REG_SZ /d \"%s\" /f"
-            % wallpaper_path
-        )
-        os.system(cmd)
-        os.system("rundll32.exe user32.dll, UpdatePerUserSystemParameters")
-        logging.info("Wallpaper is set.")
+        logging.info("No code to set wallpaper.")
+
     elif sys.platform.startswith("linux"):
-        os.system(
-            f'if [ ! -f "$XDG_RUNTIME_DIR/wayland_wallpaper" ] || [ "$(readlink -f "$XDG_RUNTIME_DIR/wayland_wallpaper")" != "$(readlink -f {wallpaper_path})" ]; then systemctl --user restart swaybg.service; fi'
+        _ = subprocess.run(
+            [
+                "sh",
+                "-c",
+                f'if [ ! -f "$XDG_RUNTIME_DIR/wayland_wallpaper" ] || [ "$(readlink -f "$XDG_RUNTIME_DIR/wayland_wallpaper")" != "$(readlink -f {wallpaper_path})" ]; then systemctl --user restart swaybg.service; fi',
+            ]
         )
 
         logging.info("Wallpaper is set.")
@@ -44,22 +42,11 @@ def set_wallpaper():
     return
 
 
-def download_old_wallpapers(minus_days=False):
-    """Uses download_wallpaper(set_wallpaper=False) to download the last 20 wallpapers.
-    If minus_days is given an integer a specific day in the past will be downloaded.
-    """
-    if minus_days:
-        download_wallpaper(idx=minus_days, use_wallpaper=False)
-        return
-    for i in range(0, 20):  # max 20
-        download_wallpaper(idx=i, use_wallpaper=False)
-
-
-def get_latest_wallpaper_date_and_url() -> Tuple[str, str]:
+def get_latest_wallpaper_date_and_url() -> tuple[str, str]:
     """Get the date and URL of the latest Bing wallpaper.
 
     Returns:
-        Tuple[str, str]: ("Date in %Y%m%d", "URL")
+        tuple[str, str]: ("Date in %Y%m%d", "URL")
     """
     url = "http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
     with contextlib.closing(request.urlopen(url)) as usock:
@@ -70,7 +57,7 @@ def get_latest_wallpaper_date_and_url() -> Tuple[str, str]:
     return response.get("startdate"), url
 
 
-def download_wallpaper(num_of_new_wallpaper=0, use_wallpaper=True):
+def download_wallpaper(num_of_new_wallpaper: int = 0):
     if not os.path.exists(WALLPAPER_DIR):
         os.makedirs(WALLPAPER_DIR)
 
@@ -120,17 +107,17 @@ def check_number_of_new_wallpapers() -> int:
     else:
         logging.error(
             "Unable to detect last download date from existing images.\n"
-            f"Last image path: {downloaded_wallpaper[-1]}\n"
+            + f"Last image path: {downloaded_wallpaper[-1]}\n"
         )
 
         exit(1)
 
     last_download_date = datetime.datetime.strptime(last_download_date, DATE_FMT).date()
-    lastest_bing_wallpaper_date, _ = get_latest_wallpaper_date_and_url()
-    lastest_bing_wallpaper_date = datetime.datetime.strptime(
-        lastest_bing_wallpaper_date, DATE_FMT
+    latest_bing_wallpaper_date, _ = get_latest_wallpaper_date_and_url()
+    latest_bing_wallpaper_date = datetime.datetime.strptime(
+        latest_bing_wallpaper_date, DATE_FMT
     ).date()
-    return (lastest_bing_wallpaper_date - last_download_date).days
+    return (latest_bing_wallpaper_date - last_download_date).days
 
 
 if __name__ == "__main__":
@@ -148,12 +135,9 @@ if __name__ == "__main__":
             logging.info(f"New wallpaper: {num_of_new_wallpaper}")
 
             if num_of_new_wallpaper > 0:
-                download_wallpaper(
-                    num_of_new_wallpaper=num_of_new_wallpaper, use_wallpaper=True
-                )
+                download_wallpaper(num_of_new_wallpaper=num_of_new_wallpaper)
 
         set_wallpaper()
 
     except Exception:
-        # os.system("notify-send -t 10000 -u critical 'Bing wallpaper download failed.'")
         raise
