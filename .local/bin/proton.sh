94471463b524d68a3ac01912096c5a7d2d727537
Hugh Wang <ihavenoname111@hotmail.com>
2026-06-27 15:30:01 +1200

    refactor(wine): Do not switch to new desktop for wineboot/winecfg/regedit

--- .local/bin/proton.sh
+++ .local/bin/proton.sh
@@ -40,7 +40,7 @@ bind_path=(
 )
 source /usr/local/share/bwrap_share/generate_args
 
-if ! [[ "$1" =~ wineboot(\.exe)? ]] && ! [[ "$1" =~ winecfg(\.exe)? ]]; then
+if ! [[ "$1" =~ wineboot(\.exe)? ]] && ! [[ "$1" =~ winecfg(\.exe)? ]] && ! [[ "$1" =~ regedit(\.exe)? ]]; then
     if pgrep -x Hyprland >/dev/null; then
         hyprctl dispatch 'hl.dsp.focus({ workspace = "empty" })' >/dev/null
     fi
--- .local/bin/wine-games.sh
+++ .local/bin/wine-games.sh
@@ -4,9 +4,10 @@ export DXVK_HDR=1
 export WINEPREFIX="$HOME/.wine/games"
 unset DISPLAY
 
-if pgrep -x Hyprland >/dev/null; then
-    hyprctl dispatch 'hl.dsp.focus({ workspace = "empty" })' >/dev/null
-
+if ! [[ "$1" =~ wineboot(\.exe)? ]] && ! [[ "$1" =~ winecfg(\.exe)? ]] && ! [[ "$1" =~ regedit(\.exe)? ]]; then
+    if pgrep -x Hyprland >/dev/null; then
+        hyprctl dispatch 'hl.dsp.focus({ workspace = "empty" })' >/dev/null
+    fi
 fi
 
 exec wine "$@"
